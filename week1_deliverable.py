9a76094f22dce2548c4bab7ad17e9825a3530641
Eesha <eeshasar@gmail.com>
2026-06-28 21:42:25 -0400

    week1_deliverable.py

--- week1_deliverable.py
+++ week1_deliverable.py
@@ -1,25 +1,34 @@
 # -*- coding: utf-8 -*-
 import pandas as pd 
 import os
-os.chdir('/Users/eeshasaraswat/Downloads/IDX Exchange Internship')
-sold1 = pd.read_csv('CRMLSSold202601.csv')
-sold2 = pd.read_csv('CRMLSSold202602.csv')
-sold3 = pd.read_csv('CRMLSSold202603.csv')
-sold4 = pd.read_csv('CRMLSSold202604.csv')
-sold5 = pd.read_csv('CRMLSSold202605.csv')
-list1 = pd.read_csv('CRMLSListing202601.csv')
-list2 = pd.read_csv('CRMLSListing202602.csv')
-list3 = pd.read_csv('CRMLSListing202603.csv')
-list4 = pd.read_csv('CRMLSListing202604.csv')
-list5 = pd.read_csv('CRMLSListing202605.csv')
-sold = pd.concat([sold1, sold2, sold3, sold4, sold5])
+folder = '/Users/eeshasaraswat/Downloads/IDX Exchange Internship' 
+all_files = os.listdir(folder)
+sold_files = [f for f in all_files if f.startswith('CRMLSSold') and f.endswith('.csv')]
+listing_files = [f for f in all_files if f.startswith('CRMLSListing') and f.endswith('.csv')]
+best_sold = []
+for f in sold_files:
+    if '_filled.csv' in f:
+        best_sold.append(f)
+    elif f.replace('.csv', '_filled.csv') not in sold_files:
+        best_sold.append(f)
+sold_dfs = []
+for f in best_sold:
+    df = pd.read_csv(os.path.join(folder, f))
+    sold_dfs.append(df)
+sold = pd.concat(sold_dfs)
 print('Sold rows after combining:', len(sold))
-listings = pd.concat([list1, list2, list3, list4, list5])
+listing_dfs = []
+for f in listing_files:
+    df = pd.read_csv(os.path.join(folder, f))
+    listing_dfs.append(df)
+listings = pd.concat(listing_dfs)
 print('Listings rows after combining:', len(listings))
 sold = sold[sold['PropertyType'] == 'Residential']
 print('Sold rows after Residential filter:', len(sold))
+
 listings = listings[listings['PropertyType'] == 'Residential']
 print('Listings rows after Residential filter:', len(listings))
-sold.to_csv('sold_combined_residential.csv', index=False)
-listings.to_csv('listings_combined_residential.csv', index=False)
+
+sold.to_csv(os.path.join(folder, 'sold_combined_residential.csv'), index=False)
+listings.to_csv(os.path.join(folder, 'listings_combined_residential.csv'), index=False)
 print('Done! Files saved.')
